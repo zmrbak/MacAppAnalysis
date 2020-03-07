259807ec3070f0eaf75ee1a43ebb4e81964f3ba4
zmrbak <20995658@qq.com>
2020-03-07 10:58:50 +0800

    add code

---
+++ 配套代码/mac22/WxTool/WxTool/NSObject+WxHelper.m
@@ -0,0 +1,19 @@
+//
+//  NSObject+WxHelper.m
+//  WxTool
+//
+//  Created by zmrbak on 2020/3/7.
+//  Copyright © 2020 zmrbak. All rights reserved.
+//
+
+#import "NSObject+WxHelper.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation NSObject (WxHelper)
++(void)zmChange
+{
+    
+}
+@end
---
+++ 配套代码/mac23/WxTool/NSObject+WxHelper.h
@@ -0,0 +1,21 @@
+//
+//  NSObject+WxHelper.h
+//  WxTool
+//
+//  Created by zmrbak on 2020/3/7.
+//  Copyright © 2020 zmrbak. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (WxHelper)
++(void)zmChange;
++ (id)zmGetLoginQRCodeResponseParseFromData:(id)arg1;
+@end
+
+NS_ASSUME_NONNULL_END
